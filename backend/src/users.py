e8d7aa7b705eb9dba18b35a73092b47bfd0b12f6
ColdCat-ship-it <michael_0409@163.com>
2023-12-12 17:10:01 +0800

    add hash password functionality

--- backend/src/users.py
+++ backend/src/users.py
@@ -1,15 +1,36 @@
 """
 This module contains the user authentication related functions.
+
+$ pip install bcrypt
+
 """
+import bcrypt as bcr
 
+import json
+from datetime import datetime, timedelta, timezone
 
-SALT = "..."
+from jwt import (
+    JWT,
+    jwk_from_dict,
+    jwk_from_pem,
+)
+from jwt.utils import get_int_from_datetime
 
-def hash_password(pw) -> str:
+#bcrypt = require('bcrypt')
+saltRounds = 10
+myPlaintextPassword = 's0/\/\P4$$w0rD'
+someOtherPlaintextPassword = 'not_bacon'
+
+SALT = "aoisdnhfsandsfasd".encode()
+
+def hash_password(pw: str) -> str:
     """
     hash the password with bcrypt
     """
-    pass
+
+    hsh = bcr.hashpw(pw.encode(),bcr.gensalt())
+
+    return str(hsh)
 
 def verify_password(pw, hash):
     """
@@ -22,3 +43,8 @@ def user_signin(name, password) -> str:
     signs in the user and returns a jwt token
     """
     pass
+
+if __name__ == "__main__":
+
+    hsh = hash_password(myPlaintextPassword)
+    print(hsh)
