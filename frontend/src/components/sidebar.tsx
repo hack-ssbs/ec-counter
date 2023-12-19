30fcad92e695931af285fc11662d870edd0bc41c
TaliaLiu <TaliaLiu@163.com>
2023-12-19 17:09:36 +0800

    update frontend ui

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -4,21 +4,23 @@ import { SidebarData } from "./SidebarData";
 
 function Bar() {
   return (
-    <div className="sidebar">
-      <ul className = "sidebarList">
+    <div className="sidebar flex-1">
+      <ul className="sidebarList">
         {SidebarData.map((val, key) => {
           return (
-            <li
-              key={key}
-              className="row"
-              id = {window.location.pathname == val.link ? "active": ""}
-              onClick={() => {
-                window.location.pathname = val.link;
-              }}
-            >
-              <div className="icon">{val.icon}</div>
-              <div className = 'title'>{val.title}</div>
-            </li>
+            <div>
+              <li
+                key={key}
+                className="row"
+                id={window.location.pathname == val.link ? "active" : ""}
+                onClick={() => {
+                  window.location.pathname = val.link;
+                }}
+              >
+                <div className="icon">{val.icon}</div>
+                <div className="title">{val.title}</div>
+              </li>
+            </div>
           );
         })}
       </ul>
