c2b15f36e8ee869ceeb54045a48da0909d3a12c6
Jett Chen <jettchen12345@gmail.com>
2023-12-20 14:12:12 +0800

    update design

--- frontend/src/Timer.tsx
+++ frontend/src/Timer.tsx
@@ -39,29 +39,35 @@ useEffect(() => {
 
   return (
     <div>
-      <h2>Energy Club</h2> 
-      <h1><strong>Volunteer Hour Counter</strong></h1>
-      <div className="clock-container">
-        <div className="clock-col">
+      <h2 className='text-2xl text-black'>Energy Club</h2> 
+      <h1 className='text-3xl text-black'>Volunteer Hour Counter</h1>
+      <div className="clock-container flex">
+        <div className="clock-col flex-1">
           <p className="clock-hours clock-timer">{String(timerHours).padStart(2, '0')}</p>
           <p className="clock-label">Hours</p>
         </div>
-        <div className="clock-col">
+        <div className="clock-col flex-1">
           <p className="clock-minutes clock-timer">{String(timerMinutes).padStart(2, '0')}</p>
           <p className="clock-label">Minutes</p>
         </div>
-        <div className="clock-col">
+        <div className="clock-col flex-1">
           <p className="clock-seconds clock-timer">{String(timerSeconds).padStart(2, '0')}</p>
           <p className="clock-label">Seconds</p>
         </div>
       </div>
       <div className="button-container">
         {!isRunning ? (
-            <button onClick={startTimer}>Start</button>
+            <button className="bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transform transition duration-150 ease-in-out hover:scale-105 focus:outline-none focus:shadow-outlin" onClick={startTimer}>
+              Start
+              </button>
           ) : (
-            <button onClick={pauseTimer}>Pause</button>
+            <button className='bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded transform transition duration-150 ease-in-out hover:scale-105 focus:outline-none focus:shadow-outline' onClick={pauseTimer}>
+              Pause
+            </button>
           )}
-          <button onClick={resetTimer}>Reset</button>
+          <button className='bg-gray-500 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded transform transition duration-150 ease-in-out hover:scale-105 focus:outline-none focus:shadow-outline' onClick={resetTimer}>
+            Reset
+          </button>
       </div>
     </div>
   );
