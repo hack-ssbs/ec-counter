33ad5eeeadfa4c49af33fc53a0817029376ed8d1
Jett Chen <jettchen12345@gmail.com>
2024-03-11 20:04:11 +0800

    update serving

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import "./App.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
