c189e22c3eb4c35c628e2bb457d5bfa60b7be4a5
Jett Chen <jettchen12345@gmail.com>
2024-04-02 23:58:28 +0800

    Initial AG Grid Integration

--- frontend/src/components/statspage.tsx
+++ frontend/src/components/statspage.tsx
@@ -1,38 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { API_PATH } from "../api";
+import { AgGridReact } from "ag-grid-react"; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Changed to Alpine Theme for a different look
+import humanizeDuration from "humanize-duration";
 
 const StatsPage: React.FC = () => {
   const [statsData, setStatsData] = useState([]);
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "username" },
+    {
+      field: "total_hours",
+      headerName: "Total VH",
+      valueGetter: (params: any) =>
+        humanizeDuration(params.data.total_hours * 3.6e6, {
+          round: true,
+          units: ["h", "m", "s"],
+          largest: 2,
+        }),
+    },
+  ]);
+
   useEffect(() => {
     fetch(`${API_PATH}/stats?jwt=${localStorage.getItem("jwt")}`)
       .then((res) => res.json())
       .then((data) => {
         setStatsData(data);
-        console.log(data);
       });
   }, []);
 
   return (
-    <div>
-      <div>
-        <h2>User Stats</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th>Total Hours</th>
-            </tr>
-          </thead>
-          <tbody>
-            {statsData.map((user, index) => (
-              <tr key={index}>
-                <td>{user["username"]}</td>
-                <td>{(user["total_hours"] as number).toFixed(2)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <h2 className="text-2xl font-bold">User Stats</h2>
+      <AgGridReact
+        rowData={statsData}
+        columnDefs={columnDefs as any}
+        domLayout="autoHeight"
+      />
     </div>
   );
 };
